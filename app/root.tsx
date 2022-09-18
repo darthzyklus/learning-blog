bbf5220d8856e0357716621a063fa538716ba4e4
Andres Uris <andres.urismontilla@gmail.com>
2022-09-18 00:52:04 -0400

    fix: highlight

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export default function App() {
           href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/tokyo-night-dark.min.css'
         />
         <script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js'></script>
-        <script>hljs.highlightAll();</script>
+        <script>hljs && hljs.highlightAll && hljs.highlightAll()</script>
         <Links />
       </head>
       <body>
