0c957856af9a2bef07fb1a7292cf20f57bf81c79
Andres Uris <andres.urismontilla@gmail.com>
2022-10-01 15:32:46 -0400

    feat: improve blog design

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,14 @@
+import { Box, Heading, Text, UnorderedList } from '@chakra-ui/react'
 import { useLoaderData } from '@remix-run/react'
-import { StructuredText } from 'react-datocms'
+import { renderNodeRule, StructuredText } from 'react-datocms'
+import { isHeading, isCode, isParagraph } from 'datocms-structured-text-utils'
 import { load } from '~/lib/datocms'
+
+/*
+ - Add a nice background
+ - Improve responsive design
+
+*/
 
 const HOMEPAGE_QUERY = `query HomePage($limit: IntType) {
   posts: allPosts(first: $limit) {
@@ -19,21 +27,41 @@ export async function loader() {
   })
 }
 
+type Post = {
+  slug: string
+  title: string
+  content: any
+}
+
 export default function Index() {
-  // TODO: add Post type
   const { posts } = useLoaderData()
 
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>TODAY I LEARNED</h1>
-      <ul>
-        {posts.map((post) => (
+    <Box
+      color='gray.200'
+      bgColor='gray.800'
+      height='100vh'
+      p={['0.4rem', '2rem', '4rem', '8rem']}
+      style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}
+    >
+      <Heading as='h1'>TODAY I LEARNED</Heading>
+      <UnorderedList m='1.2rem'>
+        {posts.map((post: Post) => (
           <li key={post.slug}>
-            <h2>{post.title}</h2>
-            <StructuredText data={post.content} />
+            <Heading as='h2' fontSize='2xl'>
+              {post.title}
+            </Heading>
+            <StructuredText
+              data={post.content}
+              customNodeRules={[
+                renderNodeRule(isParagraph, ({ children }) => {
+                  return <Text my='1.2rem'>{children}</Text>
+                }),
+              ]}
+            />
           </li>
         ))}
-      </ul>
-    </div>
+      </UnorderedList>
+    </Box>
   )
 }
